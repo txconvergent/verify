831b842136011ef82be3bc3b6772c4faa4188aa1
Anthony Lee <anthonyylee@utexas.edu>
2018-04-13 16:47:12 -0500

    update and created local mongodb in directory

--- expressServer/structure.js
+++ expressServer/structure.js
@@ -1,16 +1,5 @@
-// import { userInfo } from 'os';
-
-/*
-- /last_block --> returns the last block
-- post block --> accepts a block and appends to chain
-- save the chain in a dated folder of each instance
-
-- can the blocks be exported to json that allows res.send()
-
-- search through list to find the hash?
-*/
-
 // Libraries
+// import { userInfo } from 'os';
 const path = require('path');            // Writing file path easier
 const express = require('express');      // express server framework
 var bodyParser = require("body-parser"); // JSON parser
@@ -23,85 +12,68 @@ var port = 3000;
 app.use(bodyParser.json());  // Tell system to use JSON
 app.use(bodyParser.urlencoded( {extended: true} ));
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/<db name>")  // Need to change db name here
+mongoose.connect("mongodb://localhost:27017/imageDB");
+console.log("Connected MongoDB is running on " + mongoose.connection.host + ":" + mongoose.connection.port); // debug
+
 
 // Database (for images) document schema
-var imageSchema = new mongoose.Schema({  // Have been having issue saving, maybe its because of the schema?
-  img: { hashCode: String,
-         imageFileBinary: String
-  }
+var imageSchema = new mongoose.Schema({
+  hashCode: {type: String},
+  imageFileBinary: {type: String}
 });
 
 // Model initiation with document schemda
 var Image = mongoose.model("Image", imageSchema);
 
+// CRUD endpoints
+// app.get('/', (req, res) => res.sendFile('introPage.txt', {root: __dirname}))
 
-////////// vvvvv Testing vvvvv //////////
-mongoose.connection.on('open', function () {
-  console.error('mongo is open');
+app.get('/', (req, res) => res.sendFile('index.html', {root: __dirname}))
+app.post('/addname', (req, res) => {
+  var myData = new Image(req.body);
+  myData.save()
+    .then(item => {
+      res.send("Item saved to database");
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database.");
+    });
+});
 
-  // empty the collection
-  Image.remove(function (err) {
-    if (err) throw err;
-    console.error('removed old docs');
-
-    // store an img in binary in mongo
-    var image = new Image;
-    image.img.data = fs.readFileSync(imgPath);
-    image.img.contentType = 'image/png';
-    image.save(function (err, image) {
-      if (err) throw err;
-      console.error('saved img to mongo');
-
-// // CRUD endpoints
-// app.post("/uploadImage", (req, res) => {
-//   var myDataToBeSaved = new Image(req.image); // req.image needs to be changed to take image in some way
-//   myDataToBeSaved.save() // Saves to mongodb
-//     .then(item => {
-//       res.send("Item saved to db");
-//     })
-//     .catch(err => {
-//       res.status(400).send("Unable to save to db");
-//     }):
-// });
-
-////////// ^^^^^ Testing ^^^^^ //////////
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/', (req, res) => res.sendFile('introPage.txt', {root: __dirname}))
+// Test REST methods
 app.get('/last_block', (req, res) => res.send("This is the last block."))
 app.get('/file', (req, res) => res.sendFile('structure.js', {root: path.join(__dirname)}))
 
-app.listen(3000, () => console.log("Example app listening on port 3000!\nConnect using localhost:3000"))
+
+app.listen(port, () => {
+  console.log("Web server listening on localhost:" + port);
+});
 
 
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
 
-// Create a block with python script
-app.get('/blockchain', callPythonScript);
+// // Create a block with python script
+// app.get('/blockchain', callPythonScript);
 
-function callPythonScript(req, res) {
-  // using spawn instead of exec, prefer a stream over a buffer
-  // to avoid maxBuffer issue
+// function callPythonScript(req, res) {
+//   // using spawn instead of exec, prefer a stream over a buffer
+//   // to avoid maxBuffer issue
 
-  var spawn = require("child_process").spawn;
-  var process = spawn('python', ["./Verify.py", 
-    req.query.one,   // method one
-    req.query.two,   // method two
-    req.query.three, // method three
-    req.query.four   // method four
-  ]);
+//   var spawn = require("child_process").spawn;
+//   var process = spawn('python', ["./Verify.py", 
+//     req.query.one,   // method one
+//     req.query.two,   // method two
+//     req.query.three, // method three
+//     req.query.four   // method four
+//   ]);
 
-  process.stdout.on('data', function(data) {
-    res.send(data.toString());
-  });
-};
+//   process.stdout.on('data', function(data) {
+//     res.send(data.toString());
+//   });
+// };
+
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
