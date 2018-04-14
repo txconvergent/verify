4fbcfd1038bba8cf16394e94058501831c9f29e8
Anthony Lee <anthonyylee@utexas.edu>
2018-04-14 01:45:12 -0500

    Cleaned up code, line 54 needs resolution.

--- expressServer/structure.js
+++ expressServer/structure.js
@@ -1,5 +1,4 @@
 // Libraries
-// import { userInfo } from 'os';
 const path = require('path');            // Writing file path easier
 const express = require('express');      // express server framework
 var bodyParser = require("body-parser"); // JSON parser
@@ -8,46 +7,26 @@ var fs = require('fs');                  // To read local filesystem
 
 var app = express();
 var port = 3000;
-var blockChain = [];
+var blockchain = [];                     // Blockchain stores here
 
-app.post('/postBlock', (req, res) => {
-  // Use html id to identify hash or image
-
-  // Hash is appended to the blockchain
-  blockChain.append(req);
-
-  // Image and hash is sent to data base through Mongoose 
-});
-
-
-
-app.get('/getHashCode', (req, res) => {
-  // res.send specific blockchain block
-  // blockchain[i]
-});
-
-
-
-
-
-
-
-
-
-app.use(bodyParser.json());  // Tell system to use JSON
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/imageDB");
-console.log("Connected MongoDB is running on " + mongoose.connection.host + ":" + mongoose.connection.port); // debug
-
-/// DEBUG ///
 var db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", function(callback) {
-  console.log("Connection succeeded.");
+// Successfully connected
+db.on('connected', function() {console.log('Mongoose default connection open to ' + dbURI);});
+// Connection throws an error
+db.on('error', function(err) {console.log('Mongoose default connection error: ' + err);});
+// Connection disconnected
+db.on('disconnected', function() {console.log('Mongoose default connection disconnected');});
+// If node process ends, close the Mongoose connection.
+process.on('SIGINT', function() {
+  db.close(function() {
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
 });
-/// DEBUG ///
 
 // Database (for images) document schema
 var imageSchema = new mongoose.Schema({
@@ -58,56 +37,52 @@ var imageSchema = new mongoose.Schema({
 // Model initiation with document schemda
 var Image = mongoose.model("Image", imageSchema);
 
+
 // CRUD endpoints
-// app.get('/', (req, res) => res.sendFile('introPage.txt', {root: __dirname}))
 
-
-app.get('/', (req, res) => res.sendFile('index.html', {root: __dirname}))
-
-
-/// HARD CODE ///
-var newImage = new Image({
-  hashCode: '{type: String}',
-  imageFileBinary: 'lkjdf'
+// { hashCode: {type: String}, imageBinaryFile: {type: Float} }
+app.post('/postBlock', function(req, res) {
+  blockchain.append(res);
 });
 
-newImage.save(function(error) {
-  console.log("SAVED!");
-  if (error) {
-    console.error(error);
-  }
+// POST image to database
+app.post('/postImage', function(req, res) {
+  lastBlockHash = blockchain[-1];
+  // Create new image model with hash and imageFileBinary
+  var newImage = new Image({
+    hashCode: lastBlockHash,
+    imageFileBinary: res.body     // Need to check if body works...
+  });
+
+  // Save model to mongoDB
+  newImage.save()
+    .then(function(product) {
+      console.log("newImage is saved to database!");
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
+  
+});
+// GET hash code
+app.get('/getHashCode', (req, res) => {
+  // res.send specific blockchain block
+  // blockchain[i]
 });
 
 
-
-
-
-
-
-// app.post('/addname', (req, res) => {
-//   var
-// });
-
-// app.post('/addname', (req, res) => {
-//   var myData = new Image(req.body);
-//   myData.save()
-//     .then(item => {
-//       res.send("Item saved to database");
-//     })
-//     .catch(err => {
-//       res.status(400).send("Unable to save to database.");
-//     });
-// });
-
-// Test REST methods
-app.get('/last_block', (req, res) => res.send("This is the last block."))
-app.get('/file', (req, res) => res.sendFile('structure.js', {root: path.join(__dirname)}))
-
-
+// Listening on port
 app.listen(port, () => {
   console.log("Web server listening on localhost:" + port);
 });
 
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
+///////////// Ignore this for now /////////////
+
+// // Test REST methods
+// app.get('/last_block', (req, res) => res.send("This is the last block."))
+// app.get('/file', (req, res) => res.sendFile('structure.js', {root: path.join(__dirname)}))
 
 ///////////// Ignore this for now /////////////
 ///////////// Ignore this for now /////////////
@@ -117,13 +92,13 @@ app.listen(port, () => {
 // app.get('/blockchain', callPythonScript);
 
 // function callPythonScript(req, res) {
-//   // using spawn instead of exec, prefer a stream over a buffer
-//   // to avoid maxBuffer issue
-
-//   var spawn = require("child_process").spawn;
-//   var process = spawn('python', ["./Verify.py", 
-//     req.query.one,   // method one
-//     req.query.two,   // method two
+  //   // using spawn instead of exec, prefer a stream over a buffer
+  //   // to avoid maxBuffer issue
+  
+  //   var spawn = require("child_process").spawn;
+  //   var process = spawn('python', ["./Verify.py", 
+  //     req.query.one,   // method one
+  //     req.query.two,   // method two
 //     req.query.three, // method three
 //     req.query.four   // method four
 //   ]);
